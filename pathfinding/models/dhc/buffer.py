[Bugfix] Fix q_max calculation

pathfinding/models/dhc/buffer.py
@@ -158,7 +158,11 @@ class LocalBuffer:
 
         # caculate td errors for prioritized experience replay
         td_errors = np.zeros(self.capacity, dtype=np.float32)
-        q_max = np.max(self.q_buf[: self.size], axis=1)
+
+        q_max_idx = np.array([min(i + configs.forward_steps, self.size) for i in range(self.size)])
+        gamma = np.array([0.99 ** min(configs.forward_steps, self.size - i) for i in range(self.size)])
+        q_max = np.max(self.q_buf[q_max_idx], axis=1) * gamma
+
         fwd_steps = BUF_CONFIG["forward_steps"]
         ret = self.rew_buf.tolist() + [0 for _ in range(fwd_steps - 1)]
         reward = (

pathfinding/models/dhc/model.py
@@ -128,7 +128,7 @@ class CommBlock(nn.Module):
         if len(comm_idx) > 1:
             update_mask = update_mask.unsqueeze(2)
 
-        attn_mask = comm_mask is False
+        attn_mask = comm_mask == False
 
         for _ in range(self.num_layers):
 